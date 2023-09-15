3f1dbc3d51a1bd8ad8f29afe4d23c4b744cb12fa
saiffardin <saiffardin@gmail.com>
2023-09-15 17:33:35 +0600

    css added

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,11 @@ function App() {
 
   return (
     <div>
-      <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
-
-      {/* Routes nest inside one another. Nested route paths build upon
-            parent route paths, and nested route elements render inside
-            parent route elements. See the note about <Outlet> below. */}
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="super-heros" element={<Superheros />} />
           <Route path="rq-super-heros" element={<RQSuperheros />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
--- src/components/Layout.page.tsx
+++ src/components/Layout.page.tsx
@@ -2,9 +2,7 @@ import { Link,Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
+    <>
       <nav>
         <ul>
           <li>
@@ -24,11 +22,8 @@ const Layout = () => {
 
       <hr />
 
-      {/* An <Outlet> renders whatever child route is currently active,
-          so you can think about this <Outlet> as a placeholder for
-          the child routes we defined above. */}
       <Outlet />
-    </div>
+    </>
   );
 };
 
