9912e3d1e4092b2a89a9eedcb23c242950a14983
saiffardin <saiffardin@gmail.com>
2023-09-28 22:44:25 +0600

    add react-query library

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,24 @@ import Layout from './components/Layout.page';
 import NoMatch from './components/NoMatch.page';
 import RQSuperheros from './components/RQSuperheros.page';
 import Superheros from './components/Superheros.page';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
 
+  const queryClient = new QueryClient();  
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="super-heros" element={<Superheros />} />
-          <Route path="rq-super-heros" element={<RQSuperheros />} />
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </div>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="super-heros" element={<Superheros />} />
+            <Route path="rq-super-heros" element={<RQSuperheros />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </QueryClientProvider>
+    
   );
 }
 
--- src/components/RQSuperheros.page.tsx
+++ src/components/RQSuperheros.page.tsx
@@ -1,8 +1,33 @@
+import { useQuery } from "react-query";
+import { SUPER_HEROS } from "../utils/queryKeys";
+import axios from "axios";
+import { HeroType } from "../utils/Interface";
+
+const fetchSuperHeroes = ()=> {
+  return axios.get('http://localhost:4000/superheroes')
+}
 
 const RQSuperheros = () => {
+
+  const {isLoading, data} = useQuery(SUPER_HEROS, fetchSuperHeroes)
+
+
+  if(isLoading) {
+    return <h3>Loading...</h3>
+  }
+
   return (
     <div>
       <h1>RQSuperheros</h1>
+      
+      <ul>
+        {
+          data?.data.map((hero:HeroType)=>(
+            <li key={hero.id}>{hero.name}</li>
+          ))
+
+        }
+      </ul>
     </div>
   );
 };
